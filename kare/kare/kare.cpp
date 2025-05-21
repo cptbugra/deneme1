f61d0fe16944524f2ba93307fb0867cfdb2d3bb4
cptbugra <bugrajann26@gmail.com>
2025-05-21 23:21:15 +0300

    Add files via upload

---
+++ kare/kare/kare.cpp
@@ -0,0 +1,34 @@
+﻿
+
+#include <iostream>
+
+
+using namespace std;
+
+
+
+
+int main()
+{
+
+
+	for (int j = 1; j <= 10; j++)
+	{
+		for (int k = 1; k <= 10; k++)
+		{
+
+
+			cout << 1 << " ";
+
+		}
+		cout << endl;
+
+	}
+
+	int a;
+	cin >> a;
+
+
+
+	return 0;
+}
