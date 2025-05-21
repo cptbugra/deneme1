c45fa61886c785077bfb9ec399efede03521fbdd
cptbugra <bugrajann26@gmail.com>
2025-05-21 20:23:40 +0300

    Add files via upload

---
+++ ödev2/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,62 @@
+﻿// ConsoleApplication3.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	cout << "toplama : 1" << endl;
+	cout << "cıkarma : 2" << endl;
+	cout << "carpma : 3" << endl;
+	cout << "bolme : 4" << endl;
+
+	cin >> x;
+
+	switch (x)
+	{
+		
+		
+	case 1:
+	{
+		
+		cout << "toplama << endl";
+		break;
+
+	}
+	case 2:
+	{
+
+		cout << "cıkarma << endl";
+		break;
+
+	}
+	case 3:
+	{
+
+		cout << "carpma << endl";
+		break;
+
+	}
+	case 4:
+	{
+
+		cout << "bolme << endl";
+		break;
+
+	}
+	int a;
+	cin >> a;
+
+	return 0;
+
+
+	}
+	
+
+
+
+}
+
