a5913e3b70719a9d1efe081c2d375e44371e6b30
cptbugra <bugrajann26@gmail.com>
2025-05-21 20:35:20 +0300

    Add files via upload

---
+++ ödev4/ödev4/ödev4.cpp
@@ -0,0 +1,30 @@
+﻿// ödev4.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	int x;
+	cout << "3 haneli sayiyi giriniz" << endl;
+	cin >> x;
+
+	int yuzler = (x / 100)*100;
+	int onlar = ((x / 10) % 10)*10;
+	int birler = (x % 10);
+
+	cout << yuzler << endl;
+	cout << onlar << endl;
+	cout << birler << endl;
+
+	int a;
+	cin >> a;
+
+
+	return 0;
+}
+
+
