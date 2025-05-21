ca0e4d2e726e7fc6975ad8074f8bf5ad83d69cec
cptbugra <bugrajann26@gmail.com>
2025-05-21 23:01:25 +0300

    Add files via upload

---
+++ dik üçgen/dik üçgen/dik üçgen.cpp
@@ -0,0 +1,43 @@
+﻿
+
+#include <iostream>
+
+
+using namespace std;
+
+void deneme1(int a)
+{
+	for (int i = 1; i <= a; i++)
+	{
+
+		cout << 1<< " ";
+
+	}
+	
+
+
+
+}
+
+
+
+
+
+int main()
+{
+
+
+	for (int j = 0; j < 10; j++)
+	{
+
+		deneme1(j);
+		cout << endl;
+	}
+
+
+
+	int s;
+	cin >> s;
+
+	return 0;
+}
