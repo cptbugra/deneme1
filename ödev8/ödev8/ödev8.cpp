0dd1dc30215a9b6ffd86428b7920dac4471dcf1f
cptbugra <bugrajann26@gmail.com>
2025-05-21 21:46:12 +0300

    Add files via upload

---
+++ ödev8/ödev8/ödev8.cpp
@@ -0,0 +1,48 @@
+﻿// ödev8.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+
+    int x[5];
+    x[0] = 0;
+    x[1] = 0;
+    x[2] = 0;
+    x[3] = 0;
+    x[4] = 0;
+
+
+
+    int a;
+
+
+    do
+    {
+        for (int i = 0; i <= 4; i++)
+        {
+            cin >> a;
+            x[i] = a;
+
+
+        }
+
+        cout << "dizi:" << x[0] << x[1] << x[2] << x[3] << x[4] << endl;
+
+
+
+
+    } while (x[0] == x[1] || x[1] == x[2] || x[2] == x[3] || x[3] == x[4]);
+
+
+
+    int d;
+    cin >> d;
+
+    return 0;
+}
