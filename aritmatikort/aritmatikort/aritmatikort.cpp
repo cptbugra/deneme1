d0f1cc2befbd3b04228b7f9c09394da8daf972a7
cptbugra <bugrajann26@gmail.com>
2025-05-23 17:54:25 +0300

    Add files via upload

---
+++ aritmatikort/aritmatikort/aritmatikort.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int x[5] = { 61,64,51,28,32 };
+	int toplam = 0;
+	int a;
+	for (int i = 0; i < 5; i++)
+	{
+
+		toplam = x[i] + toplam;
+
+
+	}
+
+	a = toplam / 5;
+	cout << a;
+
+}
