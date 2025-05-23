911e1339cf98b5f108c1835c5382e8654f87b4f0
cptbugra <bugrajann26@gmail.com>
2025-05-23 16:27:12 +0300

    Add files via upload

---
+++ sıralamadizi/sıralamadizi/sıralamadizi.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int x[6] = { 1,2,3,4,5,6 };
+    int sizex = 6;
+    int sizey = 6;
+    int y[6];
+    int a;
+    cin >> a;
+    int b = a;
+
+
+    for (int i = 0; i < sizex; i++)
+    {
+
+
+        if (i == a)
+        {
+
+            y[i] = NULL;
+
+        }
+        else {
+
+            y[i] = x[i];
+        }
+
+    }
+
+
+    for (int l = a; l < sizey-1; l++)
+    {
+        y[l] = y[l + 1];
+    }
+    y[5] = 0;
+
+    cout << y[0] << y[1] << y[2] << y[3] << y[4] << y[5];
+
+    int u;
+    cin >> u;
+    
+
+}
