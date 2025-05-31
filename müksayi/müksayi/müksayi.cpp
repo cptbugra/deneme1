19ce72ba2330598ae17bd2860f0995ffce7a05da
cptbugra <bugrajann26@gmail.com>
2025-05-31 15:14:23 +0300

    Add files via upload

---
+++ müksayi/müksayi/müksayi.cpp
@@ -0,0 +1,56 @@
+﻿// müksayi.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int a;
+    cin >> a;
+    int count = 0;
+    int temp = 0;
+
+    for (int i = 1; i < a; i++)
+    {
+
+        count++;
+
+        if (a % count == 0)
+        {
+
+
+            cout << count << endl;
+            temp = temp + count;
+
+        }
+
+
+
+    }
+   
+
+
+    if (temp == a)
+    {
+
+        cout << "muk sayi:"<< temp << endl;
+        
+    }
+    else
+    {
+
+
+        cout << "muk sayi degil:" << temp << endl;
+       
+    }
+
+    
+    
+    int z;
+    cin >> z;
+}
+
+
