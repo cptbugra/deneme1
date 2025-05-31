b1809067fd436cb346fd9e8ed1d342781ebd19a5
cptbugra <bugrajann26@gmail.com>
2025-05-31 16:45:01 +0300

    Add files via upload

---
+++ kelimeuzunluk/kelimeuzunluk/kelimeuzunluk.cpp
@@ -0,0 +1,59 @@
+﻿// kelimeuzunluk.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    char* p = new char[n]();
+
+    int count = 0;
+    
+
+    
+    for (int i = 0; i < n; i++)
+    {
+
+        
+        
+        cin >> p[i];
+        
+
+
+
+    }
+
+
+    for (int i = 0; i < n; i++)
+    {
+
+
+
+        if (p[i] != NULL)
+        {
+
+            count++;
+
+
+        }
+
+
+
+
+    }
+    
+    cout << count;
+
+     
+
+
+    delete[] p;
+}
+
+
