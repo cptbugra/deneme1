9ea919eca52d5f92ae4596ca91229af274292711
cptbugra <bugrajann26@gmail.com>
2025-05-31 16:45:54 +0300

    Add files via upload

---
+++ kelimetersten/kelimetersten/kelimetersten.cpp
@@ -0,0 +1,35 @@
+﻿// kelimetersten.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    char x[20] = "hellonigga";
+    char y[20];
+
+    for (int i = 0; i < 20; i++)
+    {
+
+
+        y[i] = x[19 - i];
+
+
+
+    }
+    for (int i = 0; i < 20; i++)
+    {
+        cout << y[i];
+
+
+    }
+
+
+
+
+}
+
+
