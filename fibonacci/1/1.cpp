61660b7adf7a47c46cf16f85ad7a5053ef7ad89c
cptbugra <bugrajann26@gmail.com>
2025-05-31 15:10:10 +0300

    Add files via upload

---
+++ fibonacci/1/1.cpp
@@ -0,0 +1,30 @@
+﻿// 1.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int a = 1;
+    int b = 1;
+    int count;
+    int temp;
+
+
+    for (int i = 0; i <= 10;i++)
+    {
+        cout << a << endl;
+        int temp = a + b;
+        a = b;
+        b = temp;
+        
+
+    }
+   
+
+
+}
+
+
