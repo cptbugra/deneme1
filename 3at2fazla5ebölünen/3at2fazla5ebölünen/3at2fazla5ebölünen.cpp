6509501ba8663624665b3be9817e0a18ab883ec2
cptbugra <bugrajann26@gmail.com>
2025-05-31 15:34:03 +0300

    Add files via upload

---
+++ 3at2fazla5ebölünen/3at2fazla5ebölünen/3at2fazla5ebölünen.cpp
@@ -0,0 +1,32 @@
+﻿// 3at2fazla5ebölünen.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    for (int i = 10; i <= 200;i++)
+    {
+
+
+        if (((3 * i) + 2 )% 5 == 0)
+        {
+            
+            cout << i << endl;
+
+
+
+        }
+
+
+    }
+
+
+
+
+}
+
+
