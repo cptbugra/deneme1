0602912bdc9c5f01b5a375e1b311533f636ee553
cptbugra <bugrajann26@gmail.com>
2025-05-31 14:04:11 +0300

    Add files via upload

---
+++ transpoze/fak/fak.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+
+
+	int a[2][2] = { {4,3},{5,3} };
+	int b[2][2];
+
+	
+	for (int i = 0; i < 2;i++)
+	{
+		for (int j = 0; j < 2;j++)
+		{
+
+			b[i][j] = a[j][i];
+
+		}
+		
+
+
+	}
+	
+	cout << b[0][1] << endl;
+
+	return 0;
+}
+
+
+	
+	
+
