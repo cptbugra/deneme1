ddc55f6b94b91e80c31b5632c244a6da9d232daa
cptbugra <bugrajann26@gmail.com>
2025-05-31 20:20:57 +0300

    Add files via upload

---
+++ sirali arama(normal arama)/sirali arama(normal arama)/sirali arama(normal arama).cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int  x[10] = { 12,45,2,3,4,5,6,634,2,1 };
+    int a = 634;
+    int i = 0;
+
+    while (x[i] != a)
+    {
+
+
+        cout << i << endl;
+        i++;
+        if (x[i] == a)
+        {
+
+
+            cout << "aranan deger index : " << i << endl;
+
+
+        }
+    
+    }
+
+
+
+}
+
