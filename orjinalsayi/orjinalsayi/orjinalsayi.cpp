7e214c70d8472fd5bcdb9776db7a37fd23fbbbc2
cptbugra <bugrajann26@gmail.com>
2025-05-31 15:31:14 +0300

    Add files via upload

---
+++ orjinalsayi/orjinalsayi/orjinalsayi.cpp
@@ -0,0 +1,32 @@
+﻿// orjinalsayi.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+  
+	int x;
+	cin >> x;
+
+	int a = x / 100;
+	int b = x % 100;
+
+	if (((a + b)* (a + b)) == x)
+	{
+
+		cout << "orj sayi: " << x << endl;
+
+	}
+	else
+	{
+
+		cout << "orjinal sayi degil:" << x << endl;
+
+	}
+}
+
+
